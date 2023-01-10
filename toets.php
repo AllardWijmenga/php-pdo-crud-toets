aaf12d1ce554a2c75d4dca08e3b14ea1c943e9f6
AllardWijmenga <ra.wijmenga@gmail.com>
2023-01-10 12:15:07 +0100

    fortnite battlepass

---
+++ toets.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-pdo-crud-toets</title>
+</head>
+<body>
+    <h1>De vijf duurste Auto's ter wereld</h1>
+    <?php
+    ?>
+</body>
+</html>
